9d83722d5cfa904fcaa48336fa0b61e50c276e9d
Adam <adam.gedney@gmail.com>
2014-04-26 12:44:06 -0400

    working our pagination in get library

--- app/controllers/LibraryController.php
+++ app/controllers/LibraryController.php
@@ -43,7 +43,7 @@ class LibraryController extends BaseController {
 			'library'	=>$library,
 			'count'		=>$libraryCount,
 			'limit'  	=>$limit,
-			'skip' 		=>$page
+			'skip' 		=>(int)$page
 		);
 
 
