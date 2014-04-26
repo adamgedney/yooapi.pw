12eee0c391b8c66643f2d07e9c216eb67c5363bf
Adam <adam.gedney@gmail.com>
2014-04-26 13:03:57 -0400

    working our pagination in get library

--- app/controllers/LibraryController.php
+++ app/controllers/LibraryController.php
@@ -40,7 +40,7 @@ class LibraryController extends BaseController {
 
 
 		$obj = array(
-			(array)$library,
+			json_decode(json_encode($library), true),
 			'count'		=>$libraryCount,
 			'limit'  	=>$limit,
 			'skip' 		=>(int)$page
