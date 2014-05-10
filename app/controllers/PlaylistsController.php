b573fd4f3188ab18e8caefb6f5ad03e6ec08d9b6
Adam <adam.gedney@gmail.com>
2014-05-10 12:33:07 -0400

    testing new playlist creation method

--- app/controllers/PlaylistsController.php
+++ app/controllers/PlaylistsController.php
@@ -28,24 +28,24 @@ class PlaylistsController extends BaseController {
 			->where('user_id', '=', $userId)
 			->get();
 
-		//Store new playlist id
-		$playlistId= $getPlaylistId[0]->id;
+		// //Store new playlist id
+		// $playlistId= $getPlaylistId[0]->id;
 
-		//Generate share url
-		$shareUrl = $userId . '83027179269257243' . $playlistId;
+		// //Generate share url
+		// $shareUrl = $userId . '83027179269257243' . $playlistId;
 
-		//Add share url to new playlist entry
-		$addShareUrl = Playlists::where('id', '=', $playlistId)
-			->update(array(
-				'share_url'=>$shareUrl
-			));
+		// //Add share url to new playlist entry
+		// $addShareUrl = Playlists::where('id', '=', $playlistId)
+		// 	->update(array(
+		// 		'share_url'=>$shareUrl
+		// 	));
 
 
 
-		//Insert new playlist song on playlist id
-		$newplaylistSong = PlaylistSongs::insert(array(
-			'playlist_id'=>$playlistId,
-			'song_id'=>$songId));
+		// //Insert new playlist song on playlist id
+		// $newplaylistSong = PlaylistSongs::insert(array(
+		// 	'playlist_id'=>$playlistId,
+		// 	'song_id'=>$songId));
 
 
 
