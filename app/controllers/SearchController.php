588fa96c8ccaf144a54b7de588e64e5816612c59
Adam <adam.gedney@gmail.com>
2014-05-14 13:43:43 -0400

    wrote route to fix affiliate links in database

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -689,7 +689,7 @@ class SearchController extends BaseController {
 		$itunes = "test";
 		// $itunesArray = array();
 		//Calls Model to search DB for query
-		$itunes = Itunes::where('query', '=', '385')->get();
+		$itunes = Itunes::where('artist_view_url', 'LIKE', '%at=%')->get();
 		// $itunes = Itunes::get();
 
 			// where('artist_view_url', 'LIKE', '%http%')
