9d2c2f360e1464ef321270d38cd18316cb762e49
Adam <adam.gedney@gmail.com>
2014-05-14 15:29:30 -0400

    removed the fix method from user. All itunes urls updated in DB with affiliate id

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -682,49 +682,6 @@ class SearchController extends BaseController {
 
 
 
-	//Method to append my affiliate link to older database entries
-	public function fixAff(){
-		$pre = "4";
-		$aff = '&at=1l3vkSc';
-		$itunes = "test";
-		// $itunesArray = array();
-		//Calls Model to search DB for query
-		$itunes = Itunes::select('track_view_url')->where('track_view_url', 'LIKE', '%uo=4')->get();
-		// $itunes = Itunes::get();
-
-			// where('artist_view_url', 'LIKE', '%http%')
-				// ->
-				// ->orWhere('collection_view_url', 'LIKE', '%uo=4')
-				// ->orWhere('track_view_url', 'LIKE', '%uo=4')
-
-		foreach(json_decode($itunes) as $i){
-			// $artist = $i->artist_view_url;
-			// $album = $i->collection_view_url;
-			$track = $i->track_view_url;
-
-
-			// Itunes::where('artist_view_url', '=', $artist)
-			// ->update(array('artist_view_url'=>$artist . $aff));
-
-			// Itunes::where('collection_view_url', '=', $album)
-			// ->update(array('collection_view_url'=>$album . $aff));
-
-			Itunes::where('track_view_url', '=', $track)
-			->update(array('track_view_url'=>$track . $aff));
-
-		}
-
-		header('Access-Control-Allow-Origin: *');
-		return Response::json($itunes);
-	}
-
-
-
-
-
-
-
-
 
 
 
