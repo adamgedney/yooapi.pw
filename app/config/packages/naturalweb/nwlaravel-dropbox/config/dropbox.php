8842935b75be6f36b9b909bc9ea3fcd184622c1b
Adam <adam.gedney@gmail.com>
2014-06-24 21:55:37 -0400

    implemented naturalweb dropbox package

--- app/config/packages/naturalweb/nwlaravel-dropbox/config/dropbox.php
+++ app/config/packages/naturalweb/nwlaravel-dropbox/config/dropbox.php
@@ -2,11 +2,11 @@
 
 /*
 |--------------------------------------------------------------------------
-| Configuration Dropbox    my access token cruL1114F7kAAAAAAAAdYESQf-tpPbff6_H39rb2zO4t1mFrvHsDmd00YHEIomAx
+| Configuration Dropbox   secret: w0wsbrmyudg2ehb  my access token cruL1114F7kAAAAAAAAdYESQf-tpPbff6_H39rb2zO4t1mFrvHsDmd00YHEIomAx
 |--------------------------------------------------------------------------
 */
 
 return array(
-    'token'  => 'w0wsbrmyudg2ehb',
+    'token'  => 'cruL1114F7kAAAAAAAAdYESQf-tpPbff6_H39rb2zO4t1mFrvHsDmd00YHEIomAx',
     'app'    => 'aus1ebbm53ym50j',
 );
